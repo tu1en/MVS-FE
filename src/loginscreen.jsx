c1bdc8a6c93f5ab2c9f24a3314df74ee57d3d799
turmatoes <nguyenxuanphilong@gmail.com>
2025-05-25 17:22:23 +0700

    reset password done

--- src/loginscreen.jsx
+++ src/loginscreen.jsx
@@ -91,7 +91,7 @@ export default function LoginScreen() {
                 Password
               </label>
               <div className="text-sm">
-                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                <a href="/forgot-password" className="font-semibold text-indigo-600 hover:text-indigo-500">
                   Forgot password?
                 </a>
               </div>
