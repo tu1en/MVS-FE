1ce30f4c107a1486af3eab3b0cff1669e7ffc1b1
Tuong2k <daniel.alex2k3@gmail.com>
2025-05-26 08:00:27 +0700

    add dashboard page, update login page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
-import Layout from './components/Layout';
-import AssignmentsPage from './pages/AssignmentsPage';
-import BlankPage from './pages/BlankPage';
-import ClassesPage from './pages/ClassesPage';
-import HomePage from './pages/HomePage';
-import StudentsPage from './pages/StudentsPage';
-import LoginScreen from './pages/loginscreen.jsx';
-import ForgotPassword from './components/ForgotPassword';
-import ResetPassword from './components/ResetPassword';
-import { useEffect } from 'react';
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import "./App.css";
+import Layout from "./components/Layout";
+import AssignmentsPage from "./pages/AssignmentsPage";
+import BlankPage from "./pages/BlankPage";
+import ClassesPage from "./pages/ClassesPage";
+import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/Dashboard";
+import StudentsPage from "./pages/StudentsPage";
+import LoginScreen from "./pages/LoginScreen";
+import ForgotPassword from "./components/ForgotPassword";
+import ResetPassword from "./components/ResetPassword";
+import { useEffect } from "react";
 
 /**
  * Main App component
@@ -19,14 +24,21 @@ import { useEffect } from 'react';
  */
 function App() {
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
-      const role = localStorage.getItem('role');
-      switch(role) {
-        case 'ADMIN': window.location.href = '/admin'; break;
-        case 'TEACHER': window.location.href = '/teacher'; break;
-        case 'STUDENT': window.location.href = '/student'; break;
-        default: window.location.href = '/';
+      const role = localStorage.getItem("role");
+      switch (role) {
+        case "ADMIN":
+          window.location.href = "/admin";
+          break;
+        case "TEACHER":
+          window.location.href = "/teacher";
+          break;
+        case "STUDENT":
+          window.location.href = "/student";
+          break;
+        default:
+          window.location.href = "/";
       }
     }
   }, []);
@@ -34,7 +46,8 @@ function App() {
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<DashboardPage />} />
           <Route path="/classes" element={<ClassesPage />} />
           <Route path="/assignments" element={<AssignmentsPage />} />
           <Route path="/students" element={<StudentsPage />} />
