f2dac3845f69b80f610075e5d94b5a036d84c47e
vutlhe160502 <vutlhe160502@fpt.edu.vn>
2025-07-21 23:25:57 +0700

    add contract

--- src/App.js
+++ src/App.js
@@ -92,6 +92,7 @@ import ResetPasswordPage from "./pages/ResetPasswordPage.jsx"; // Import a new p
 import StudentAccomplishments from './pages/StudentAccomplishments.jsx';
 import TestUpload from "./pages/TestUpload.jsx";
 import { ensureRoleConsistency } from "./utils/authUtils.js";
+import ContractManagement from './pages/accountant/ContractManagement';
 import AccountantLeaveRequest from './pages/accountant/AccountantLeaveRequest';
 
 // A component to redirect based on user role
@@ -265,6 +266,7 @@ function App() {
 
               {/* Accountant Routes */}
               <Route path="/accountant" element={<ProtectedRoute allowedRoles={["ACCOUNTANT"]}><AccountantDashboard /></ProtectedRoute>} />
+              <Route path="/accountant/contracts" element={<ProtectedRoute allowedRoles={["ACCOUNTANT"]}><ContractManagement /></ProtectedRoute>} />
               <Route path="/accountant/leave-requests" element={<ProtectedRoute allowedRoles={["ACCOUNTANT"]}><AccountantLeaveRequest /></ProtectedRoute>} />
 
               {/* Test Upload Page */}
