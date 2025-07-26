19f3ee7c0068386c1b59b8e3f5641159e7022ff8
turmatoes <nguyenxuanphilong@gmail.com>
2025-07-26 19:33:56 +0700

    recruit logic fix

--- src/components/RecruitmentApplyForm.jsx
+++ src/components/RecruitmentApplyForm.jsx
@@ -21,6 +21,8 @@ const RecruitmentApplyForm = ({ job, onSuccess }) => {
       formData.append('jobPositionId', job.id);
       formData.append('email', values.email);
       formData.append('fullName', values.fullName);
+      formData.append('phoneNumber', values.phoneNumber);
+      formData.append('address', values.address);
       if (values.cv && values.cv.length > 0) {
         formData.append('cv', values.cv[0].originFileObj);
       }
@@ -37,48 +39,61 @@ const RecruitmentApplyForm = ({ job, onSuccess }) => {
   };
 
   return (
-    <Form
-      form={form}
-      layout="vertical"
-      onFinish={handleFinish}
-      autoComplete="off"
-    >
+    <Form form={form} onFinish={handleFinish} layout="vertical">
       <Form.Item
         name="fullName"
         label="Họ và tên"
-        rules={[{ required: true, message: 'Vui lòng nhập họ tên' }]}
+        rules={[{ required: true, message: 'Vui lòng nhập họ tên!' }]}
       >
-        <Input placeholder="Nhập họ tên đầy đủ" />
+        <Input />
       </Form.Item>
+
       <Form.Item
         name="email"
         label="Email"
         rules={[
-          { required: true, message: 'Vui lòng nhập email' },
-          { type: 'email', message: 'Email không hợp lệ' },
+          { required: true, message: 'Vui lòng nhập email!' },
+          { type: 'email', message: 'Email không hợp lệ!' }
         ]}
       >
-        <Input placeholder="Nhập email của bạn" />
+        <Input />
       </Form.Item>
+
+      <Form.Item
+        name="phoneNumber"
+        label="Số điện thoại"
+        rules={[{ required: true, message: 'Vui lòng nhập số điện thoại!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="address"
+        label="Địa chỉ"
+        rules={[{ required: true, message: 'Vui lòng nhập địa chỉ!' }]}
+      >
+        <Input.TextArea rows={3} />
+      </Form.Item>
+
       <Form.Item
         name="cv"
         label="Tải lên CV"
         valuePropName="fileList"
         getValueFromEvent={normFile}
-        rules={[{
-          validator: (_, value) =>
-            value && value.length > 0
-              ? Promise.resolve()
-              : Promise.reject('Vui lòng upload file CV'),
-        }]}
+        rules={[{ required: true, message: 'Vui lòng tải lên CV!' }]}
       >
-        <Upload beforeUpload={() => false} maxCount={1} accept=".pdf,.doc,.docx">
-          <Button icon={<UploadOutlined />}>Chọn file</Button>
+        <Upload
+          beforeUpload={() => false}
+          accept=".pdf,.doc,.docx"
+          maxCount={1}
+        >
+          <Button icon={<UploadOutlined />}>Chọn file CV</Button>
         </Upload>
       </Form.Item>
+
       <Form.Item>
-        <Button type="primary" htmlType="submit" loading={uploading} block size="large">
-          Gửi CV ứng tuyển
+        <Button type="primary" htmlType="submit" loading={uploading} block>
+          Nộp đơn ứng tuyển
         </Button>
       </Form.Item>
     </Form>
